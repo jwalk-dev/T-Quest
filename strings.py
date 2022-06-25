7d482b10706cfe179f226a74902a625f5cd0de10
Jake Walker <jacob.fletcher.walker@gmail.com>
2022-06-25 11:06:40 -0400

    checkin

---
+++ strings.py
@@ -0,0 +1,6 @@
+import sys
+
+input_string = sys.argv[1]
+output_string = input_string.upper()+ "!!!"
+
+print(output_string)
---
+++ variables.py
@@ -0,0 +1,5 @@
+favorite_robot = "Cedric"
+meaning_of_life = 42 
+
+print (f"my favorite robot is {favorite_robot}")
+print (f"meaning of life is {meaning_of_life}")
